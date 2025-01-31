85ba96b5c53b7d80f00dadb9a87043b372921a3d
bakape <bakape@gmail.com>
2025-02-01 01:06:52 +0200

    Add basic Nix flake for development

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    utils.url = "github:numtide/flake-utils";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, utils, nixpkgs, fenix, devshell }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlays.default devshell.overlays.default ];
+        };
+        inherit (pkgs) lib stdenv;
+        rustPackageSet = with pkgs.fenix;
+          combine [
+            stable.cargo
+            stable.clippy
+            stable.rustc
+            stable.rust-src
+            latest.rustfmt
+          ];
+      in {
+        formatter = pkgs.nixpkgs-fmt;
+        devShells.default = pkgs.devshell.mkShell {
+          imports = [ "${devshell}/extra/language/rust.nix" ];
+          packages = with pkgs; [
+            rustPackageSet
+            rust-analyzer-nightly
+            clang
+            nixfmt
+          ];
+          language.rust = {
+            enableDefaultToolchain = false;
+            packageSet = rustPackageSet;
+          };
+          env = with pkgs; [{
+            name = "RUST_SRC_PATH";
+            value =
+              "${pkgs.fenix.stable.rust-src}/lib/rustlib/src/rust/library";
+          }];
+        };
+      });
+}
